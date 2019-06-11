3bc691c70378f09a722aee68712647bdccfa67eb
unknown <nd.managment@gmail.com>
2019-06-11 22:33:26 +0200

    defont

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,12 +11,7 @@ module.exports = {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       },
-      resolve: 'gatsby-plugin-web-font-loader',
-      options: {
-        google: {
-          families: ['Quattrocento', 'Fanwood Text']
-        }
-      },
+      
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
